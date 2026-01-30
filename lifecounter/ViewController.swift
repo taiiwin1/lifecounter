acdcad87c72f26c22970a86ca23181a3ca0aeb02
Tai Nguyen <tainnguyen2@gmail.com>
2026-01-29 16:12:53 -0800

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Tai Nguyen on 1/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
